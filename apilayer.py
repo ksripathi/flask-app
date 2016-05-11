8e9c3d7ec3faeebf3f7cb1fc6bae2f69c36fd42f
ksripathi <kammari.sripathi@gmail.com>
2016-05-11 09:35:03 +0530

    Added templates

--- apilayer.py
+++ apilayer.py
@@ -1,30 +1,45 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, render_template, request, session, redirect
 import json
 from flask_sqlalchemy import SQLAlchemy
 from dblayer import *
 from utils import jsonify_list
 app = Flask(__name__)
+app.secret_key = 'djfjsdkjXXS7979dfdfd'
 #SQLALCHEMY_DATABASE_URI = 'mysql+oursql://root:vlead123@localhost/demo'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+oursql://root:vlead123@localhost/demo'
 
+@app.route("/")
+def index():
+    return render_template('index.html')
 
-@app.route("/users/<username>")
-def hello(username):
-    try:
-        user = User.query.filter_by(username=username).first_or_404()
+@app.route("/login", methods=['GET', 'POST'])
+def hello():
+    if request.method == 'GET':
+        if ('email' in session):
+            return render_template('success.html')
+        else:
+            return redirect('/')
+        
+    else:
+        get_name = request.form['text']
+        name = str(get_name)
+        get_pswd = request.form['pswd']
+        pswd = str(get_pswd)
+        user = User.query.filter_by(username=name).first_or_404()
         if user.username:
-            return jsonify({'Found the result for the name': user.username})
-    except:
-        return "failure"
+            session['email'] = user.email
+            return render_template('success.html', name=session['email'], paswd=pswd)
 
+@app.route("/logout", methods=['GET'])
+def logout():
+    session.pop('email', None)
+    return redirect("/")
+#    return render_template('index.html')
+'''
 @app.route("/users")
 def users():
     users = User.query.all()
     return jsonify_list([i.to_client() for i in users])
-#    return make_response(json.dumps(users), 200,
- #                        {'content-type': 'application/json'})
-
-    
-
+'''
 if __name__ == "__main__":
     app.run(debug=True)
